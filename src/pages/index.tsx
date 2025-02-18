e464ac5687a3737af355ccd719fe7392f4086784
ndevfr <ndev@mailo.com>
2025-02-18 11:17:09 +0100

    feat: update icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,20 @@ export default function Home(props: StaticProps) {
         <title>NDev.fr</title>
         <meta name="description" content="Visitez mon portfolio personnel" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/icons/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/icons/site.webmanifest" />
       </Head>
       <div
         id="detection"
